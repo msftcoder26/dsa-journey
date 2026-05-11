684909badfd5294d310778c10436d2e0f5f17363
Vidisha Karn <vidishakarn365@gmail.com>
2026-05-11 22:27:02 +0530

    Day 1 completed

---
+++ DAY 1/1.Input/Snippet.cpp
@@ -0,0 +1,19 @@
+//| Snippet | Use               |
+// | ------- | ----------------- |
+// | `fori`  | loops             |
+// | `vin`   | vector input      |
+// | `sortv` | sorting           |
+// | `func`  | function          |
+// | `maxe`  | max element       |
+// | `map`   | hashmap/frequency |
+// | `stack` | stack questions   |
+
+
+// | Shortcut          | Use              |
+// | ----------------- | ---------------- |
+// | `Ctrl + /`        | comment line     |
+// | `Alt + ↑ / ↓`     | move line        |
+// | `Shift + Alt + ↓` | duplicate line   |
+// | `Ctrl + D`        | select same word |
+// | `Ctrl + Space`    | suggestions      |
+// | `Tab`             | expand snippet   |
