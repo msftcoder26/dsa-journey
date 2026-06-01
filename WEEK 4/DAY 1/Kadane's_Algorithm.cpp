04a656362558443e4abb17e75c17092b311b7c45
Vidisha Karn <vidishakarn365@gmail.com>
2026-06-01 09:49:03 +0000

    Kadane's Algorithm

--- WEEK 4/DAY 1/Kadane's_Algorithm.cpp
+++ WEEK 4/DAY 1/Kadane's_Algorithm.cpp
@@ -45,6 +45,53 @@ int maxSubArray(vector<int>& nums){
     return maxi;
 }
 
+//print the subarray
+int maxSubArray(vector<int>& nums){
+
+    int sum = 0;
+
+    int maxi = INT_MIN;
+
+    int start = 0;
+
+    int ansStart = -1;
+    int ansEnd = -1;
+
+    for(int i=0; i<nums.size(); i++){
+
+        if(sum == 0){
+
+            start = i;
+        }
+
+        sum += nums[i];
+
+        if(sum > maxi){
+
+            maxi = sum;
+
+            ansStart = start;
+            ansEnd = i;
+        }
+
+        if(sum < 0){
+
+            sum = 0;
+        }
+    }
+
+    cout << "Subarray: ";
+
+    for(int i=ansStart; i<=ansEnd; i++){
+
+        cout << nums[i] << " ";
+    }
+
+    cout << endl;
+
+    return maxi;
+}
+
 int main(){
 
     int n;
