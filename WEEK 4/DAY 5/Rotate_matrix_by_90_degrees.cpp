a30fe801e8753bc216dc4e673868d7945a703a56
Vidisha Karn <vidishakarn365@gmail.com>
2026-06-05 07:10:39 +0000

    Rotate Image

---
+++ WEEK 4/DAY 5/Rotate_matrix_by_90_degrees.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//brute
+void rotate(vector<vector<int>>& matrix){
+
+    int n = matrix.size();
+
+    vector<vector<int>> ans(n, vector<int>(n));
+
+    for(int i=0; i<n; i++){
+
+        for(int j=0; j<n; j++){
+
+            ans[j][n-1-i] = matrix[i][j];
+        }
+    }
+
+    matrix = ans;
+}
+
+//optimal
+void rotate(vector<vector<int>>& matrix){
+
+    int n = matrix.size();
+
+    // Transpose matrix
+    for(int i=0; i<n; i++){
+
+        for(int j=i+1; j<n; j++){
+
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+
+    // Reverse each row
+    for(int i=0; i<n; i++){
+
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    
+
+    return 0;
+} 
