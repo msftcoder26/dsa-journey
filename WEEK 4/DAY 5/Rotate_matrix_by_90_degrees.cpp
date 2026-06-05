bfceb1e0cce96ff83a248426ad2d1df0a963aeae
Vidisha Karn <vidishakarn365@gmail.com>
2026-06-05 07:36:02 +0000

    Rotate Image

--- WEEK 4/DAY 5/Rotate_matrix_by_90_degrees.cpp
+++ WEEK 4/DAY 5/Rotate_matrix_by_90_degrees.cpp
@@ -44,7 +44,30 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    
+    int n;
+    cin >> n;
+
+    vector<vector<int>> matrix(n, vector<int>(n));
+
+    for(int i=0; i<n; i++){
+
+        for(int j=0; j<n; j++){
+
+            cin >> matrix[i][j];
+        }
+    }
+
+    rotate(matrix);
+
+    for(int i=0; i<n; i++){
+
+        for(int j=0; j<n; j++){
+
+            cout << matrix[i][j] << " ";
+        }
+
+        cout << "\n";
+    }
 
     return 0;
 } 
