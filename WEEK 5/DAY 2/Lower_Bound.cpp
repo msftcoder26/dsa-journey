34f4dbfc080c4722b482ca068c49e8ce814d05a7
Vidisha Karn <vidishakarn365@gmail.com>
2026-06-09 12:27:21 +0000

    Lower Bound

---
+++ WEEK 5/DAY 2/Lower_Bound.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//brute
+int lowerBound(vector<int>& arr, int x){
+
+    int n = arr.size();
+
+    for(int i=0; i<n; i++){
+
+        if(arr[i] >= x){
+
+            return i;
+        }
+    }
+    return n;
+}
+
+//optimal
+int lowerBound(vector<int>& arr, int target){
+
+    int n = arr.size();
+
+    int low = 0;
+    int high = n - 1;
+
+    int ans = n;
+
+    while(low <= high){
+
+        int mid = (low + high)/2;
+
+        if(arr[mid] >= target){
+            ans = mid;
+            high = mid - 1;
+        }
+
+        else{
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int x;
+    cin >> x;
+
+    cout << lowerBound(arr, x);
+
+    return 0;
+}
