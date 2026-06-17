e081f3b9f93878fb8c10b576c244829da1a93e2f
Vidisha Karn <vidishakarn365@gmail.com>
2026-06-17 15:13:39 +0000

    Book Allocation

---
+++ WEEK 6/DAY 3/Book_Allocation.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//helper code
+/*If a student can read at most "pages",
+how many students are required?*/
+int countSt(vector<int>& arr, int barrier){
+
+    int studentCount = 1;
+    int currentStudentPages = 0;
+
+    for(int i = 0; i < arr.size(); i++){
+
+        if(currentStudentPages + arr[i] <=barrier){
+            currentStudentPages += arr[i];
+        }
+        else{
+            studentCount++;
+            currentStudentPages = arr[i];
+        }
+    }
+    return studentCount;
+}
+
+int bookAllocation(vector<int>& arr, int totalStudents){
+
+    int totalarr = arr.size();
+
+    if(totalStudents > totalarr){
+        return -1;
+    }
+
+    int low = *max_element(arr.begin(), arr.end());
+
+    int high = accumulate(arr.begin(), arr.end(), 0);
+
+    while(low <= high){
+
+        int mid = (low + high)/2;
+
+        int requiredStudents = countSt(arr, mid);
+
+        // Barrier too small
+        if(requiredStudents > totalStudents){
+            low = mid + 1;
+        }
+
+        // Barrier works
+        else{
+            high = mid - 1;
+        }
+    }
+    return low;
+}
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    int m;
+    cin >> m;
+
+    cout << bookAllocation(arr, m);
+
+    return 0;
+}
