35491195fee23a60ea314344a014f9705fca6ec8
Vidisha Karn <vidishakarn365@gmail.com>
2026-07-04 07:15:06 +0000

    Palindrome of LL

--- WEEK 8/DAY 4/Palindrome.cpp
+++ WEEK 8/DAY 4/Palindrome.cpp
@@ -13,25 +13,22 @@ public:
     }
 };
 
+//Step 1: Reverse
 Node* reverse(Node* head){
 
     Node* prev = nullptr;
     Node* curr = head;
 
     while(curr != nullptr){
-
         Node* front = curr->next;
-
         curr->next = prev;
-
         prev = curr;
-
         curr = front;
     }
-
     return prev;
 }
 
+//Step 2: Find middle (Tortoise & Hare)
 bool isPalindrome(Node* head){
 
     if(head == nullptr || head->next == nullptr){
@@ -42,25 +39,21 @@ bool isPalindrome(Node* head){
     Node* fast = head;
 
     while(fast->next != nullptr && fast->next->next != nullptr){
-
         slow = slow->next;
         fast = fast->next->next;
     }
 
+    //Step 3: compare
     Node* secondHalf = reverse(slow->next);
-
     Node* firstHalf = head;
 
     while(secondHalf != nullptr){
-
         if(firstHalf->data != secondHalf->data){
             return false;
         }
-
         firstHalf = firstHalf->next;
         secondHalf = secondHalf->next;
     }
-
     return true;
 }
 
